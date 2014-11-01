a0d698708c1feae04ff83f5e38e41549ad93a6a8
fgcui <fgcui1204@163.com>
2014-11-02 00:25:24 +0800

    modify compare method

--- src/main/java/com/thoughtworks/iamcoach/guessnumber/CompareNumber.java
+++ src/main/java/com/thoughtworks/iamcoach/guessnumber/CompareNumber.java
@@ -2,6 +2,18 @@ package com.thoughtworks.iamcoach.guessnumber;
 
 public class CompareNumber {
     public String compare(String answer, String input) {
-        return "4A0B";
+
+        int numberA = 0;
+        int numberB = 0;
+
+        for (int i = 0; i < answer.length(); i++){
+            if((answer.charAt(i)+"").equals(input.charAt(i)+"")){
+                numberA += 1;
+            }else if(answer.contains(input.charAt(i)+"")){
+                numberB += 1;
+
+            }
+        }
+        return numberA+"A"+numberB+"B";
     }
 }
