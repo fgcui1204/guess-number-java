d45ca49617b9d3a4b996b5f8792be559d6a454ef
fgcui <fgcui1204@163.com>
2014-11-02 00:10:53 +0800

    modify compare method

--- src/main/java/com/thoughtworks/iamcoach/guessnumber/CompareNumber.java
+++ src/main/java/com/thoughtworks/iamcoach/guessnumber/CompareNumber.java
@@ -2,6 +2,6 @@ package com.thoughtworks.iamcoach.guessnumber;
 
 public class CompareNumber {
     public String compare(String answer, String input) {
-        return null;
+        return "4A0B";
     }
 }
