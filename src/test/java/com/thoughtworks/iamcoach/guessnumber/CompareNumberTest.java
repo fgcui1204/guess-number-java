cb1976aed6d8a7f0c43e67a4d83f524f88f64cf8
fgcui <fgcui1204@163.com>
2014-11-02 00:12:15 +0800

    modify test

--- src/test/java/com/thoughtworks/iamcoach/guessnumber/CompareNumberTest.java
+++ src/test/java/com/thoughtworks/iamcoach/guessnumber/CompareNumberTest.java
@@ -1,16 +1,24 @@
 package com.thoughtworks.iamcoach.guessnumber;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
 public class CompareNumberTest {
+
+    private CompareNumber compareNumber;
+
+    @Before
+    public void before(){
+        compareNumber = new CompareNumber();
+    }
+
     @Test
     public void result_length_should_4(){
-        CompareNumber compareNumber = new CompareNumber();
-
         String result = compareNumber.compare("1234","1243");
 
         assertThat(result.length()).isEqualTo(4);
     }
+    
 }
