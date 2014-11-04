657667295d4115e74741a66e31fac0cc345baf60
fgcui <fgcui1204@163.com>
2014-11-04 11:07:06 +0800

    add test result_length_should_be_4 method

---
+++ src/main/java/com/thoughtworks/iamcoach/guessnumber/AnswerGenerator.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.iamcoach.guessnumber;
+
+public class AnswerGenerator {
+    public String generate() {
+        return null;
+    }
+}
--- src/test/java/com/thoughtworks/iamcoach/guessnumber/AnswerGeneratorTest.java
+++ src/test/java/com/thoughtworks/iamcoach/guessnumber/AnswerGeneratorTest.java
@@ -1,7 +1,18 @@
 package com.thoughtworks.iamcoach.guessnumber;
 
-/**
- * Created by cuifugang on 14-11-3.
- */
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+
 public class AnswerGeneratorTest {
+
+    @Test
+    public void result_length_should_be_4(){
+
+        AnswerGenerator answerGenerator = new AnswerGenerator();
+        String result = answerGenerator.generate();
+
+        assertThat(result.length()).isEqualTo(4);
+    }
 }
