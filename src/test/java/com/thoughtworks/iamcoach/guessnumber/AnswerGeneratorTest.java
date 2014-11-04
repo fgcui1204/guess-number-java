470fefa9ec4eec687a65a87efe1b36d582ddd9af
fgcui <fgcui1204@163.com>
2014-11-04 11:08:21 +0800

    modify test

--- src/test/java/com/thoughtworks/iamcoach/guessnumber/AnswerGeneratorTest.java
+++ src/test/java/com/thoughtworks/iamcoach/guessnumber/AnswerGeneratorTest.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.iamcoach.guessnumber;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.fest.assertions.api.Assertions.assertThat;
@@ -7,12 +8,19 @@ import static org.fest.assertions.api.Assertions.assertThat;
 
 public class AnswerGeneratorTest {
 
+    private AnswerGenerator answerGenerator;
+    @Before
+    public void init(){
+        answerGenerator = new AnswerGenerator();
+    }
+
     @Test
     public void result_length_should_be_4(){
 
-        AnswerGenerator answerGenerator = new AnswerGenerator();
         String result = answerGenerator.generate();
 
         assertThat(result.length()).isEqualTo(4);
     }
+
+
 }
